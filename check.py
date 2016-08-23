a32489fd45e6f1fa4a1c04ad5d5dd3ea614a68e7
Sam <342447974@qq.com>
2016-08-24 01:54:10 +0800

    fix

---
+++ check.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+import hashlib
+import os
+import time
+
+
+
+def getHash(f):
+    line = f.readline()
+    hash = hashlib.md5()
+    while (line):
+        hash.update(line)
+        line = f.readline()
+    return hash.hexdigest()
+
+
+def IsHashEqual(f1, f2):
+    str1 = getHash(f1)
+    str2 = getHash(f2)
+    return str1 == str2
+
+
+if __name__ == '__main__':
+    # os.system('cd check_web')
+    os.system('scrapy crawl check_web_sprider')
+    print '0s'
+    time.sleep(10)
+    print '10s'
+    f1 = open("./test.json", "rb")
+    f2 = open("./test1.json", "rb")
+
+    if (IsHashEqual(f1, f2) is False):
+        def _format_addr(s):
+            name, addr = parseaddr(s)
+            return formataddr(( \
+                Header(name, 'utf-8').encode(), \
+                addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+
+        from_addr = 'mclaren1234@163.com'
+        password = 'ly19940306'
+        to_addr = '342447974@qq.com'
+        smtp_server = 'smtp.163.com'
+
+        msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+        msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
+        msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
+        msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
+
+        server = smtplib.SMTP(smtp_server, 25)
+        server.set_debuglevel(1)
+        server.login(from_addr, password)
+        server.sendmail(from_addr, [to_addr], msg.as_string())
+        server.quit()
---
+++ check_web/pipelines.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import json
+import codecs
+import sys
+import os
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+class CheckWebPipeline(object):
+    def __init__(self):
+        if(os.path.isfile('test.json')):
+            self.file = codecs.open('test1.json', mode='wb', encoding='utf-8')
+        else:
+            self.file = codecs.open('test.json', mode='wb', encoding='utf-8')
+        # self.file = codecs.open('test.json', mode='wb', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        line = 'the new movie list:' + '\n\n\n'
+        for i in range(len(item['body'])):
+            body = {'body': str(item['body'][i]).replace(' ', '')}
+            line = line + json.dumps(body, ensure_ascii=False) + '\n'
+        self.file.write(line)
+
+        # return item
+
+    def close_spider(self, spider):
+        self.file.close()
